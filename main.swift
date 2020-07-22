97edd923bf793d0e8719beba6b017b19ef92de90
Adrian Sampson <adrian@radbox.org>
2020-07-22 16:38:14 -0400

    Fiddle with main

--- main.swift
+++ main.swift
@@ -1,17 +1,22 @@
 import Foundation
 
-let manager = OrbManager()
-manager.start() { orb in
-    orb.getColor() { color in
-        print(color)
-    }
-
-    orb.setColor((
+func randColor() -> Color {
+    return (
         255,
         UInt8.random(in: 0...255),
         UInt8.random(in: 0...255),
         UInt8.random(in: 0...255)
-    ))
+    )
+}
+
+let manager = OrbManager()
+manager.start() { orb in
+    orb.getColor() { color in
+        print(color)
+        orb.setColor((255, color.g, color.b, color.r))
+    }
+
+    // orb.setColor(randColor())
 }
 
 while RunLoop.current.run(
